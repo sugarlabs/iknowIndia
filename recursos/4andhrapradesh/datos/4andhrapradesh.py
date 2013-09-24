f4640babf2328b573e109371eae03f11f7367178
Alan Aguiar <alanjas@hotmail.com>
2013-09-24 07:51:47 -0300

    add andhra pradesh

---
+++ recursos/4andhrapradesh/datos/4andhrapradesh.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+from gettext import gettext as _
+
+NAME = _('Andhra Pradesh')
+
+STATES = [
+    (_('Adilabad'), 254, 223, 140, 0),
+    (_('Nizamabad'), 253, 140, 219, 0),
+    (_('Karimnagar'), 252, 238, 214, 0),
+    (_('Medak'), 251, 145, 284, 0),
+    (_('Warangal'), 250, 304, 277, 0),
+    (_('Rangareddy'), 249, 136, 336, 0),
+    (_('Nalgonda'), 248, 253, 367, 0),
+    (_('Khammam'), 247, 394, 312, 0),
+    (_('Mahabubnagar'), 246, 137, 418, 0),
+    (_('Kamulu'), 245, 115, 514, 0),
+    (_('Ananthapur'), 244, 77, 613, 0),
+    (_('Kadapa'), 243, 201, 624, 0),
+    (_('Chittoor'), 242, 231, 719, 0),
+    (_('Nelluru'), 241, 295, 627, 0),
+    (_('Prakasam'), 240, 268, 517, 0),
+    (_('Guntur'), 239, 341, 437, 0),
+    (_('Krishna'), 238, 401, 412, -45),
+    (_('West Godavari'), 237, 458, 381, -45),
+    (_('East Godavari'), 236, 514, 339, 0),
+    (_('Vishakhapatnam'), 235, 573, 284, 0),
+    (_('Vijayanagaram'), 234, 642, 220, 0),
+    (_('Srikakulam'), 233, 701, 213, 0),
+    # States
+    (_('Orissa'), 232, 668, 88, 0),
+    (_('Chhattisgarh'), 231, 437, 176, 0),
+    (_('Maharashtra'), 230, 143, 45, 0),
+    (_('Karnataka'), 229, 65, 750, 0),
+    (_('Tamil Nadu'), 228, 198, 846, 0),
+    (_('Andaman and Nicobar'), 227, 686, 539, 90)    
+]
+
+CAPITALS = []
+
+CITIES = []
+
+RIVERS = []
+
+ROUTES = []
+
+STATS = [
+    (_('Capital:'), _('Hyderabad')),
+    (_('Area:'), _('275.045 km²')),
+    (_('Population:'), _('75.727.541'))
+]
---
+++ recursos/4andhrapradesh/datos/explorations.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+from gettext import gettext as _
+
+EXP1 = [
+    _('Divisions'),
+    ['lineasDepto'],
+    [],
+    ['deptos']
+]
+
+EXP2 = [
+    _('Taluka Headquarters'),
+    ['lineasDepto', 'capitales'],
+    [],
+    ['capitales']
+]
+
+EXP3 = [
+    _('Cities'),
+    ['lineasDepto', 'capitales', 'ciudades'],
+    [],
+    ['capitales', 'ciudades']
+]
+
+EXP4 = [
+    _('Waterways'),
+    ['rios'],
+    [],
+    ['rios']
+]
+
+EXP5 = [
+    _('Routes'),
+    ['rutas', 'capitales'],
+    ['capitales'],
+    ['rutas']
+]
+
+EXPLORATIONS = [EXP1]
+
